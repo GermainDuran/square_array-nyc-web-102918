749d71ca69310b4da7e44a7d343c09d890c2e01f
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-11 19:15:24 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
+
 def square_array(array)
-  # your code here
-  array.each do |num|
-    array2=square_array(array)
+  new_array = []
+  array.each do |num| new_array << num ** 2 
   end
-  
-  return array2
+  new_array
 end
